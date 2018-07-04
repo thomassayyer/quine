f92ac9a5c5ce82d7ac69aaba986cc94dde027beb
Thomas SAYER <thomas.sayyer@gmail.com>
2018-07-04 18:39:05 +0200

    Refactoring

--- src/application/controllers/InGameController.java
+++ src/application/controllers/InGameController.java
@@ -6,40 +6,9 @@ import java.util.List;
 import application.models.Card;
 import application.models.Partner;
 
+/**
+ * Contrôleur de la page "En jeu".
+ */
 public class InGameController extends Controller {
-
-	// Liste des cartons des joueurs absents
-	private List<Card> absentBuyerCard;
-
-	// Liste des nombres
-	private LinkedList<Integer> numbers;
-
-	// Liste des partenaires
-	private List<Partner> partners;
-
-	/**
-	 * Instance Singleton du contrôleur
-	 */
-	private static InGameController instance = null;
-
-	/**
-	 * Crée une nouvelle instance du controleur.
-	 */
-	private InGameController() {
-	}
-
-	/**
-	 * Retourne l'instance Singleton du contrôleur.
-	 *
-	 * @return L'instance Singleton du contrôleur
-	 */
-	public static InGameController getInstance() {
-
-		if (instance == null) {
-			instance = new InGameController() {
-			};
-		}
-
-		return instance;
-	}
+    // TODO: Réfléchir aux méthodes à implémenter.
 }
